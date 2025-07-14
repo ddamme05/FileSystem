b602122dbc24444577c4369d163d380adf64c0e4
ddamme05 <ddamme05@gmail.com>
2025-07-13 21:59:43 -0400

    Added User level operations and a DTO.

---
+++ src/main/java/org/ddamme/dto/RegisterRequest.java
@@ -0,0 +1,18 @@
+package org.ddamme.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+} 
