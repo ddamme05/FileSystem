9a4b1fb80ca594a97371394167465aec20b8e72f
ddamme05 <ddamme05@gmail.com>
2025-08-02 01:23:00 -0400

    Edited expiration time to 24h instead of 24min.

--- src/main/java/org/ddamme/security/service/JwtService.java
+++ src/main/java/org/ddamme/security/service/JwtService.java
@@ -33,7 +33,7 @@ public class JwtService {
         return Jwts.builder()
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // 24 hours
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 hours
                 .signWith(secretKey)
                 .compact();
     }
