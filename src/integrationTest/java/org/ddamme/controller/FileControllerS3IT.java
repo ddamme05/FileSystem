5a5a2d7d2351d40b575bae709a7b01fd804f810a
ddamme05 <ddamme05@gmail.com>
2025-08-10 00:04:12 -0400

    [TEST-03] Fixed imports.

--- src/integrationTest/java/org/ddamme/controller/FileControllerS3IT.java
+++ src/integrationTest/java/org/ddamme/controller/FileControllerS3IT.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
